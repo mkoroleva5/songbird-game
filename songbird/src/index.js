3baed759ba81472ec275b685e168d916aaa11529
Maria Koroleva <mkoroleva5@gmail.com>
2022-11-09 20:28:32 +0300

    feat: add birds gallery page

--- songbird/src/index.js
+++ songbird/src/index.js
@@ -5,13 +5,26 @@ console.log(birdsData)
 // ---------- Game start ----------
 
 const playButton = document.querySelector('.play-button');
+const homeLink = document.querySelector('.home-link');
 const gameLink = document.querySelector('.game-link');
+const galleryLink = document.querySelector('.gallery-link');
 const startPage = document.querySelector('.start-section');
 const gamePage = document.querySelector('.game-section');
+const galleryPage = document.querySelector('.gallery-section');
+const resultsPage = document.querySelector('.results-section');
+
+homeLink.addEventListener('click', () => {
+    startPage.style.display = 'flex';
+    resultsPage.style.display = 'none';
+    gamePage.style.display = 'none';
+    galleryPage.style.display = 'none';
+});
 
 playButton.addEventListener('click', () => {
     setTimeout(() => {
         startPage.style.display = 'none';
+        galleryPage.style.display = 'none';
+        resultsPage.style.display = 'none';
         gamePage.style.display = 'flex';
     }, 500);  
 });
@@ -19,27 +32,38 @@ playButton.addEventListener('click', () => {
 gameLink.addEventListener('click', () => {
     setTimeout(() => {
         startPage.style.display = 'none';
+        resultsPage.style.display = 'none';
+        galleryPage.style.display = 'none';
         gamePage.style.display = 'flex';
     }, 500);  
 });
+
+galleryLink.addEventListener('click', () => {
+    startPage.style.display = 'none';
+    resultsPage.style.display = 'none';
+    gamePage.style.display = 'none';
+    galleryPage.style.display = 'flex';
+});
+
 
 // ---------- Levels ----------
 
 const levelInputs = document.querySelectorAll('.level-input');
 const levelLabels = document.querySelectorAll('.level-label');
-const levelsForm = document.querySelector('.levels-form');
-/*
-function changeLevels() {
-    for (let i = 0; i < levelInputs.length; i++) {
-        levelLabels[i].style.backgroundColor = '#9dbd00';
-        if (levelInputs[i].checked) {
-            levelLabels[i].style.backgroundColor = '#b7d428';
-        }
-    }
-}*/
 
 if (levelInputs[0].checked) {
     levelLabels[0].style.backgroundColor = '#b7d428';
+}
+
+function changeLevels() {
+    for (let i = 0; i < levelInputs.length; i++) {
+        if (levelInputs[i].checked) {
+            levelLabels[i].style.backgroundColor = '#9dbd00';
+            levelLabels[i+1].style.backgroundColor = '#b7d428';
+            levelInputs[i+1].checked = 'true';
+            break;
+        }
+    }
 }
 /*
 levelsForm.addEventListener('input', () => {
@@ -61,6 +85,7 @@ const questionImage = document.querySelector('.question-bird-image');
 const questionName = document.querySelector('.question-bird-name');
 const questionSong = document.querySelector('.question-song');
 import unknownBird from './assets/unknown-bird.jpg';
+import { result } from "lodash";
 
 let answer;
 function getRandomSong(levelNum) {
@@ -99,13 +124,17 @@ for (let i = 0; i < answerInputs.length; i++) {
     });
 }
 
+let counter = 6;
 function selectAnswers() {
+    counter--;
     for (let i = 0; i < answerInputs.length; i++) {
         if (answerInputs[i].checked) {
             if (answerLabels[i].textContent === answer.name) {
                 answerLabels[i].firstElementChild.style.backgroundColor = '#9dbd00';
                 questionImage.style.backgroundImage = `url('${answer.image}')`;
                 questionName.innerHTML = answer.name;
+                score.innerHTML = +score.innerHTML + counter;
+                counter = 6;
                 nextLevelButton.classList.add('active');
                 answersForm.removeEventListener('input', selectAnswers);
             } else {
@@ -117,29 +146,29 @@ function selectAnswers() {
 
 answersForm.addEventListener('input', selectAnswers);
 
-function changeLevels() {
-    for (let i = 0; i < levelInputs.length; i++) {
-        if (levelInputs[i].checked) {
-            levelLabels[i].style.backgroundColor = '#9dbd00';
-            levelLabels[i+1].style.backgroundColor = '#b7d428';
-            levelInputs[i+1].checked = 'true';
-            break;
-        }
-    }
-}
-
 nextLevelButton.addEventListener('click', () => {
     if (nextLevelButton.classList.contains('active')) {
-        nextLevelButton.classList.remove('active');
-        changeLevels();
-        for (let i = 0; i < levelInputs.length; i++) {
-            if (levelInputs[i].checked) {
-                getRandomSong(i);
-                createAnswers();
-                answersForm.addEventListener('input', selectAnswers);
+        if (!levelInputs[5].checked) {
+            nextLevelButton.classList.remove('active');
+            changeLevels();
+            for (let i = 0; i < levelInputs.length; i++) {
+                if (levelInputs[i].checked) {
+                    getRandomSong(i);
+                    createAnswers();
+                    answersForm.addEventListener('input', selectAnswers);
+                }
             }
+            console.log(answer.name)
+        } else {
+            console.log('win')
+            setTimeout(() => {
+                gamePage.style.display = 'none';
+                resultsPage.style.display = 'flex';
+                resultMessage.innerHTML = `Поздравляем!\nВы прошли викторину и набрали ${score.innerHTML} из 30 возможных баллов`
+            }, 500);
+            
+            //----------дописать страницу результатов!----------------------
         }
-        console.log(answer.name)
     }
 })
 
@@ -160,18 +189,107 @@ function createAnswers() {
                 birdSong.innerHTML = '';
 
                 answerLabels[j].addEventListener('click', () => {
-                    let bird = birdsData[i].find(item => item.name === answerLabels[j].textContent);
-                    birdImage.style.backgroundImage = `url('${bird.image}')`;
-                    birdImage.style.border = '5px solid rgb(255, 250, 234, 0.5)';
-                    birdName.innerHTML = bird.name;
-                    birdName.style.borderBottom = '1px solid #d0d0d0';
-                    birdLatinName.innerHTML = bird.species;
-                    birdLatinName.style.borderBottom = '1px solid #d0d0d0';
-                    birdDescription.innerHTML = bird.description;
-                    birdSong.innerHTML = `<audio class="bird-audio" controls src="${bird.audio}">`;
+                    if (levelInputs[i].checked) {
+                        let bird = birdsData[i].find(item => item.name === answerLabels[j].textContent);
+                        birdImage.style.backgroundImage = `url('${bird.image}')`;
+                        birdImage.style.border = '5px solid rgb(255, 250, 234, 0.5)';
+                        birdName.innerHTML = bird.name;
+                        birdName.style.borderBottom = '1px solid #d0d0d0';
+                        birdLatinName.innerHTML = bird.species;
+                        birdLatinName.style.borderBottom = '1px solid #d0d0d0';
+                        birdDescription.innerHTML = bird.description;
+                        birdSong.innerHTML = `<audio class="bird-audio" controls src="${bird.audio}">`;
+                    }
                 });
             }
         }
     }
 }
 createAnswers();
+
+// ---------- Score ----------
+
+const score = document.querySelector('.score-number');
+const resultMessage = document.querySelector('.result');
+score.innerHTML = 0;
+
+// ---------- Gallery ----------
+
+const birdTypesForm = document.querySelector('.bird-types-form');
+const birdTypeInputs = document.querySelectorAll('.bird-type-input');
+const birdTypeLabels = document.querySelectorAll('.bird-type-label');
+const gallery = document.querySelector('.gallery');
+
+if (birdTypeInputs[0].checked) {
+    birdTypeLabels[0].style.backgroundColor = '#b7d428';
+}
+
+birdTypesForm.addEventListener('input', () => {
+    for (let i = 0; i < birdTypeInputs.length; i++) {
+        birdTypeLabels[i].style.backgroundColor = '#9dbd00';
+        if (birdTypeInputs[i].checked) {
+            birdTypeLabels[i].style.backgroundColor = '#b7d428';
+            birdTypeInputs[i].checked = 'true';
+            generateBirdCards(i);
+        }
+    }
+})
+
+for (let i = 0; i < birdTypeLabels.length; i++) {
+    birdTypeLabels[i].addEventListener('mouseover', () => {
+        birdTypeLabels[i].style.backgroundColor = '#b7d428';
+    });
+    birdTypeLabels[i].addEventListener('mouseout', () => {
+        if (!birdTypeInputs[i].checked) birdTypeLabels[i].style.backgroundColor = '#9dbd00';
+    });
+}
+
+function generateBirdCards(type) {
+    gallery.innerHTML = '';
+    for (let i = 0; i < birdTypeInputs.length; i++) {
+
+        let birdCard = document.createElement('div');
+        birdCard.classList.add('bird-card');  
+        gallery.appendChild(birdCard);
+
+        let birdCardImage = document.createElement('div');
+        birdCardImage.classList.add('bird-image');
+        birdCardImage.style.backgroundImage = `url('${birdsData[type][i].image}')`;
+        birdCardImage.style.border = '5px solid rgb(255, 250, 234, 0.5)';
+        birdCard.appendChild(birdCardImage);
+        
+        let birdCardName = document.createElement('div');
+        birdCardName.classList.add('bird-name');
+        birdCardName.innerHTML = birdsData[type][i].name;
+        birdCardName.style.borderBottom = '1px solid #d0d0d0';
+        birdCard.appendChild(birdCardName);
+
+        let birdCardLatinName = document.createElement('div');
+        birdCardLatinName.classList.add('bird-latin-name');
+        birdCardLatinName.innerHTML = birdsData[type][i].species;
+        birdCardLatinName.style.borderBottom = '1px solid #d0d0d0';
+        birdCard.appendChild(birdCardLatinName);
+
+        let birdCardSong = document.createElement('audio');
+        birdCardSong.classList.add('bird-audio');
+        birdCardSong.setAttribute('controls', 'true');
+        birdCardSong.setAttribute('src', birdsData[type][i].audio);
+        birdCard.appendChild(birdCardSong);
+
+        let birdCardDescr = document.createElement('div');
+        birdCardDescr.classList.add('bird-description');
+        birdCardDescr.style.display = 'none';
+        birdCardDescr.innerHTML = birdsData[type][i].description;
+
+        birdCard.appendChild(birdCardDescr);
+
+        birdCard.addEventListener('click', () => {
+            birdCard.classList.toggle('bird-card-active');
+            if (!birdCard.classList.contains('bird-card-active')) birdCardDescr.style.display = 'none';
+            else birdCardDescr.style.display = 'block';
+        });
+    }   
+}
+
+generateBirdCards(0);
+
